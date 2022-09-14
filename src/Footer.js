a4f422b9151ef27b2aa8c53dd1a83ef60fde0eef
Hailemichael Demelash <hayilemekaledemelash1@gmail.com>
2022-09-14 10:36:35 -0700

    First Commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <h1 className='footer'>
+      Built By: Hailemichael Demelash
+    </h1>
+  );
+}
+
+export default Footer;
